920bbb20a01529655327d4fadff56229ca0495f7
Joe Bender <joebenderweb@gmail.com>
2020-01-15 14:19:16 -0500

    Add category data creation to data-create.py

--- data-create.py
+++ data-create.py
@@ -8,24 +8,32 @@ NUM_SAMPLES = 3
 
 # create data.npy file to store the entire dataset
 data_dir = os.path.join('asl-alphabet', 'asl_alphabet_train', 'asl_alphabet_train')
-X_list = []
+Xs_list = []
+ys_list = []
+
 categories = os.listdir(data_dir)
+# create category-to-integer index
+cat2idx = {cat: idx for idx, cat in enumerate(categories)}
 
 for category in categories:
     cat_dir = os.path.join(data_dir, category)
     image_files = os.listdir(cat_dir)
+    y_idx = cat2idx[category]
     
     for image_file in image_files[:NUM_SAMPLES]:
         path = os.path.join(data_dir, category, image_file)
         image = cv2.imread(path)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        X_list.append([gray])
+        Xs_list.append([gray])
+        ys_list.append(y_idx)
 
-X = np.vstack(X_list)
-
+Xs = np.vstack(Xs_list)
+ys = np.vstack(ys_list)
 
 # save dataset to file
 np_dir = 'numpy_data'
 os.mkdir(np_dir)
-X_path = os.path.join(np_dir, 'X')
-np.save(X_path, X)
+Xs_path = os.path.join(np_dir, 'Xs')
+np.save(Xs_path, Xs)
+ys_path = os.path.join(np_dir, 'ys')
+np.save(ys_path, ys)
