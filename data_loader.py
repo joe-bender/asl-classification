62f62799da517499debeb1516cd160738f3e0d37
Joe Bender <joebenderweb@gmail.com>
2020-01-17 11:34:47 -0500

    Add get_batch_gen to data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -23,7 +23,7 @@ def get_datasets(train=True):
         return Xs, ys
 
 # randomly draw batch_size number of samples from the given dataset
-def get_batch(Xs, ys, batch_size=8):
+def get_batch_random(Xs, ys, batch_size=8):
     # number of inputs and labels should match
     assert(len(Xs) == len(ys))
     sample_idxs = random.sample(range(len(ys)), batch_size)
@@ -31,3 +31,16 @@ def get_batch(Xs, ys, batch_size=8):
     Xs_batch = Xs[sample_idxs, :]
     ys_batch = ys[sample_idxs]
     return Xs_batch, ys_batch
+
+# get batches from a generator, to cover all samples each epoch
+def get_batch_gen(Xs, ys, batch_size=8, odd_batch=False):
+    num_samples = len(ys)
+    sample_idxs = list(range(num_samples))
+    random.shuffle(sample_idxs)
+    if odd_batch:
+        end = num_samples+1
+    else:
+        end = num_samples+1-batch_size
+    for start in range(0, end, batch_size):
+        batch_idxs = sample_idxs[start:start+batch_size]
+        yield Xs[batch_idxs, :], ys[batch_idxs]
